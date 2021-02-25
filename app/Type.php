1bd176cc011280a5ee78a855f4ca9b28e9e9c97e
Luca Ripa <71653890+RipaLuca@users.noreply.github.com>
2021-02-25 16:13:53 +0100

    Update Type.php
    Erreur de frappe "O" -->"o"

--- app/Type.php
+++ app/Type.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Type extends Model
 {
-     public function Offre()
+     public function offre()
     {
         return $this->hasMany('App\Offre');
     }

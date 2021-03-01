9a0c445c7da79c8aa79bf5d75acbb9eb8a44bfcc
Dorian39 <76092790+Dorian39@users.noreply.github.com>
2021-03-01 16:02:19 +0100

    Adaptation du template (Texte par défault

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -35,25 +35,23 @@
     <!-- Fixed navbar -->
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Fixed navbar</a>
+        <a class="navbar-brand" href="#">JobFinder</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
           <ul class="navbar-nav me-auto mb-2 mb-md-0">
             <li class="nav-item active">
-              <a class="nav-link" aria-current="page" href="#">Home</a>
+              <a class="nav-link" aria-current="page" href="#">Offres</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            <li class="nav-item active">
+              <a class="nav-link" aria-current="page" href="#">Emplois</a>
             </li>
           </ul>
           <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
+          <li class="nav-item">
+              <a class="nav-link" href="#">Déconnexion</a>
+            </li>
           </form>
         </div>
       </div>
@@ -63,21 +61,20 @@
   <!-- Begin page content -->
   <main class="flex-shrink-0">
     <div class="container">
-      <h1 class="mt-5">Sticky footer with fixed navbar</h1>
-      <p class="lead">Pin a footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code class="small">padding-top: 60px;</code> on the <code class="small">main &gt; .container</code>.</p>
-      <p>Back to <a href="/docs/5.0/examples/sticky-footer/">the default sticky footer</a> minus the navbar.</p>
+      <h1 class="mt-5">Bienvenue sur le site <b>JobFinder</b></h1>
+      <p class="lead">Vous pouvez naviguer sur les différents onglets pour trouver des offres d'emplois</p>
     </div>
   </main>
 
   <footer class="footer mt-auto py-3 bg-light">
     <div class="container">
-      <span class="text-muted">Place sticky footer content here.</span>
+      <span class="text-muted">Footer</span>
     </div>
   </footer>
 
 
       <script src="/public/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
-        
+
   </body>
 </html>

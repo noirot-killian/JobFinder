660547bbfb10260b31f3c4b5f989e6655031b41f
Luca Ripa <71653890+RipaLuca@users.noreply.github.com>
2021-03-03 09:07:51 +0100

    Verification relation et composer auth
    Vérification avec tinker des relations ainsi que l'installation de auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('template');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');

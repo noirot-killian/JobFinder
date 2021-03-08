63c5f23b8f8d52510dde4f8c7feab6e42fbf0af1
NOIROT Killian <noirotkillian@gmail.com>
2021-03-08 08:36:29 +0100

    Update vue "display_offres"
    Ajout d'un bouton "Postuler" et d'un bouton "Mettre en favoris".

--- resources/views/offres/display_offres.blade.php
+++ resources/views/offres/display_offres.blade.php
@@ -6,18 +6,20 @@
 
 @section('content')
 	<div class="card text-center">
-    <div class="card-header">
-     	Détails de l'offre
-    </div>
-    <div class="card-body">
-    <h5 class="card-title">{{$o->intitule}}</h5>
-    <p class="card-text">{{$o->description}}</p>
-    <p class="card-text">{{$o->durée}}</p>
-    <p class="card-text">{{$o->entreprise}}</p>
-    <p class="card-text">{{$o->ville}}</p>
-    <p class="card-text">Contact : {{$o->contact}}</p>
-    <p class="card-text">PDF : {{$o->PDF}}</p>
-    <a href="{{route('offre.index')}}" class="btn btn-primary">Retour</a>
-    </div>
+        <div class="card-header">
+         	Détails de l'offre
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">{{$o->intitule}}</h5>
+            <p class="card-text">{{$o->description}}</p>
+            <p class="card-text">{{$o->durée}}</p>
+            <p class="card-text">{{$o->entreprise}}</p>
+            <p class="card-text">{{$o->ville}}</p>
+            <p class="card-text">Contact : {{$o->contact}}</p>
+            <p class="card-text">PDF : {{$o->PDF}}</p>
+            <a href="#" class="btn btn-primary">Postuler</a>
+            <a href="#" class="btn btn-primary">Mettre en favoris</a>
+            <a href="{{route('offre.index')}}" class="btn btn-primary">Retour</a>
+        </div>
 	</div>
 @stop
